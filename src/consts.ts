1c7af18a2eb7ee965bd0bcd6e97eebac23901611
Stefan Utamaru <github.le5ke@stenyan.jp>
2025-03-29 23:23:57 +0900

    rename blog title to stenyan.dev

--- src/consts.ts
+++ src/consts.ts
@@ -13,7 +13,7 @@ export type Link = {
 }
 
 export const SITE: Site = {
-  TITLE: '/^(fa){3}$/',
+  TITLE: 'stenyan.dev',
   DESCRIPTION: 'すてにゃん (stefafafan) によるテックブログです。',
   NUM_POSTS_ON_HOMEPAGE: 5,
   POSTS_PER_PAGE: 5,
